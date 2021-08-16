ad9152e483ae0cb3fe20c5ad7ea2c11929fae9c4
vhubli <vhubli@LIN25005238.corp.capgemini.com>
2021-08-16 13:55:57 +0530

    Sales Class commit.

---
+++ SampleApp/src/main/java/SalesClass.java
@@ -0,0 +1,11 @@
+
+public class SalesClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, I am from sales class");
+		System.out.println("Hello, how do you do?");
+		System.out.println("Hello all");
+
+	}
+
+}
